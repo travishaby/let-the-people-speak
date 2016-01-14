02c5b29cb92d828e38655441d5c0e54b36e810a2
Travis Haby <travishaby@gmail.com>
2016-01-14 13:19:05 -0700

    feature: admin and poll links dynamically populate based on app root

--- public/admin.js
+++ public/admin.js
@@ -1,1 +1,11 @@
-console.log("admin js file loaded");
+$(document).ready(function(){
+  var pollUrlLink = $('#poll-url');
+  var pollLinkWithRoot = $(location).attr('origin') + "/" + pollUrlLink.text();
+  pollUrlLink.text(pollLinkWithRoot);
+  pollUrlLink.attr('href', pollLinkWithRoot);
+
+  var adminUrlLink = $('#admin-url');
+  var adminLinkWithRoot = $(location).attr('href');
+  adminUrlLink.text(adminLinkWithRoot);
+  adminUrlLink.attr('href', adminUrlLink.text());
+})
