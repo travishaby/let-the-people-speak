c6ee2a87e1dd8e1db620dbe993fc923875b164a3
Travis Haby <travishaby@gmail.com>
2016-01-15 11:46:12 -0700

    refactor: dataStore now takes in poll params sent in post request and creates a poll internally

--- dataStore.js
+++ dataStore.js
@@ -1,7 +1,14 @@
 const _ = require('lodash');
+const Poll = require('./poll');
 
 function DataStore() {
   this.polls = {};
+}
+
+DataStore.prototype.createPoll = function(pollParams) {
+  var poll = new Poll(pollParams);
+  this.polls[poll.admin_id] = poll;
+  return poll;
 }
 
 DataStore.prototype.findPollByAdminId = function(id) {
