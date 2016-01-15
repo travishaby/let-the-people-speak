310f2a6d5eaeb245ffd4f46b6b927352e308f9c7
Travis Haby <travishaby@gmail.com>
2016-01-15 16:27:25 -0700

    refactor: removed unecessary document ready trigger. also added poll-specific channel for sending votes message

--- public/admin.js
+++ public/admin.js
@@ -1,17 +1,20 @@
-$(document).ready(function(){
-  var pollUrlLink = $('#poll-url');
-  var pollLinkWithRoot = $(location).attr('origin') + "/" + pollUrlLink.text();
-  pollUrlLink.text(pollLinkWithRoot);
-  pollUrlLink.attr('href', pollLinkWithRoot);
+var pollUrlLink = $('#poll-url');
+var pollLinkWithRoot = $(location).attr('origin') + "/" + pollUrlLink.text();
+pollUrlLink.text(pollLinkWithRoot);
+pollUrlLink.attr('href', pollLinkWithRoot);
 
-  var adminUrlLink = $('#admin-url');
-  var adminLinkWithRoot = $(location).attr('href');
-  adminUrlLink.text(adminLinkWithRoot);
-  adminUrlLink.attr('href', adminUrlLink.text());
-})
+var adminUrlLink = $('#admin-url');
+var adminLinkWithRoot = $(location).attr('href');
+adminUrlLink.text(adminLinkWithRoot);
+adminUrlLink.attr('href', adminUrlLink.text());
 
 var socket = io();
 
 socket.on('usersConnected', function(count) {
   console.log('Connected users: ' + count);
 });
+
+var pollId = $('#poll-id').text();
+socket.on('pollResponse-' + pollId, function(pollResponses){
+  console.log(pollResponses);
+})
--- public/poll.js
+++ public/poll.js
@@ -4,26 +4,25 @@ socket.on('usersConnected', function(count) {
   console.log('Connected users: ' + count);
 });
 
-$(document).ready(function(){
-  $('.poll-question').on('click', function() {
-    var pollResponse = $(this).text();
-    sendPollResponse(pollResponse);
-  });
+$('.poll-question').on('click', function() {
+  var pollResponse = $(this).text();
+  sendPollResponse(pollResponse);
 });
-
-socket.on('pollResponses', function(pollResponses){
-  console.log(pollResponses);
-})
 
 Array.prototype.last = function(){
   return this[this.length - 1];
 };
 
+var pollId = window.location.pathname.split('/').last();
+
 function sendPollResponse(pollResponse) {
-  var pollId = window.location.pathname.split('/').last();
   socket.send('voteCast', {
           pollId: pollId,
     pollResponse: pollResponse,
        responder: socket.id
   });
-}
+};
+
+socket.on('pollResponse-' + pollId, function(pollResponses){
+  console.log(pollResponses);
+});
