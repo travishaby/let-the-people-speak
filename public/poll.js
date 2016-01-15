bd7e7f911bdbcf9968817301be99466d2ca58ada
Travis Haby <travishaby@gmail.com>
2016-01-14 20:10:20 -0700

    feature: client grabs pollResponse when button is clicked and sends response to server response is accompanied by the poll ID so when multiple polls are open the server can keep track of which responses received through the sockets correspond to which polls

--- public/poll.js
+++ public/poll.js
@@ -1,8 +1,27 @@
-
-
-
 var socket = io();
 
 socket.on('usersConnected', function(count) {
   console.log('Connected users: ' + count);
 });
+
+$(document).ready(function(){
+  $('.poll-question').on('click', function() {
+    var pollResponse = $(this).text();
+    sendPollResponse(pollResponse);
+  });
+});
+
+if (!Array.prototype.last){ //create last() method but don't overwrite if extant
+    Array.prototype.last = function(){
+        return this[this.length - 1];
+    };
+};
+
+function sendPollResponse(pollResponse) {
+  var pollId = window.location.pathname.split('/').last();
+  console.log(pollResponse, pollId);
+  socket.send('voteCast', {
+          pollId: pollId,
+    pollResponse: pollResponse
+  });
+}
