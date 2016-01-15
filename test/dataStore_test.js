79519e05f324d4c6345c1f81041b5ed40ec0d34b
Travis Haby <travishaby@gmail.com>
2016-01-15 10:44:12 -0700

    pass tests: poll object has hash to save polls in and can find polls by both admin_id and poll_id

--- test/dataStore_test.js
+++ test/dataStore_test.js
@@ -2,13 +2,36 @@
 var chai = require('chai');
 var expect = chai.expect;
 var should = chai.should();
-const DataStore = require('../dataStore')
+const DataStore = require('../dataStore');
+const Poll = require('../poll');
 
 var dataStore = new DataStore
 
 describe('dataStore.js', function () {
   it('has a polls attribute that starts as an empty object', function (done) {
     expect(dataStore.polls).eql({});
+    done();
+  });
+  it('polls are saved in polls by their admin_id', function (done) {
+    var pollParams = { name: 'test poll',
+      questions: {
+        question1: 'question1'
+      }
+    }
+    var poll = new Poll(pollParams);
+    dataStore.polls[poll.admin_id] = poll;
+    expect(dataStore.findPollByAdminId(poll.admin_id)).eql(poll);
+    done();
+  });
+  it('can find saved polls by the poll_id', function (done) {
+    var pollParams = { name: 'test poll',
+      questions: {
+        question1: 'question1'
+      }
+    }
+    var poll = new Poll(pollParams);
+    dataStore.polls[poll.admin_id] = poll;
+    expect(dataStore.findPollByPollId(poll.poll_id)).eql(poll);
     done();
   });
 })
