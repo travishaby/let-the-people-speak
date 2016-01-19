0f898956f091384fd385b772d44caf306c0e3764
Travis Haby <travishaby@gmail.com>
2016-01-19 12:59:20 -0700

    write/pass test: feature test confirms voting for a poll from poll view and seeing results populate immediately on poll page

--- test/feature/feature_tests.js
+++ test/feature/feature_tests.js
@@ -22,5 +22,24 @@ module.exports = {
       .assert.containsText('#admin-url', '/admin/')
       .assert.containsText('#close-poll', 'Close Poll')
       .end();
+  },
+  'Vote from poll view, see results populate in poll and admin views': function (browser) {
+    browser
+      .url('http://localhost:8080')
+      .waitForElementVisible('body', 1000)
+      .setValue('input[name="poll[question]"]', 'test question')
+      .setValue('input[name="poll[choices][choice1]"]', 'test choice one')
+      .setValue('input[name="poll[choices][choice2]"]', 'test choice two')
+      .click('input[class="checkbox form-control"]')
+      .waitForElementVisible('input[value="Create Poll"]', 1000)
+      .click('input[value="Create Poll"]')
+      .waitForElementVisible('#poll-url', 1000)
+      .click('#poll-url')
+      .assert.urlContains('/poll/')
+      .assert.containsText('button[name="button"]', 'test choice one')
+      .click('button[name="button"]')
+      .waitForElementVisible('table[class="table table-striped"]', 1000)
+      .assert.containsText('table[class="table table-striped"]', 'test choice one')
+      .end();
   }
 };
